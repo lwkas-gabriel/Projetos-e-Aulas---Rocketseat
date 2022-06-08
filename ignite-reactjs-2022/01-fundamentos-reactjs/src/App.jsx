8cd6dec9045c04c54b308df654c2a9aa79ce50ac
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-06-08 17:16:57 -0300

    componente post finalizado

--- ignite-reactjs-2022/01-fundamentos-reactjs/src/App.jsx
+++ ignite-reactjs-2022/01-fundamentos-reactjs/src/App.jsx
@@ -1,29 +1,23 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 
 import "./global.css"
 
 import styles from "./App.module.css"
-import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
-      
+
       <Header />
 
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Lucas Gabriel"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus repudiandae id nesciunt ad dignissimos at iure aliquid soluta ullam perspiciatis."
-          />
-
-          <Post 
-            author="Teste"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, minus?"
-          />
+          <Post />
+          <Post />
+          <Post />
         </main>
       </div>
 
