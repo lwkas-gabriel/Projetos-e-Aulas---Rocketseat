743951dc97d5bd08355bd103c58ef3d4b62f2d89
lwkas-gabriel <lwkasgabriel@hotmail.com>
2022-06-09 20:09:14 -0300

    validação form comentário

--- ignite-reactjs-2022/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite-reactjs-2022/01-fundamentos-reactjs/src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Esse campo é obrigatório!");
     }
 
     function deleteComment(commentToDelete) {
@@ -39,6 +44,8 @@ export function Post({ author, publishedAt, content }) {
         });
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -73,10 +80,12 @@ export function Post({ author, publishedAt, content }) {
                     value={newCommentText}
                     placeholder="Deixe um comentário"
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
