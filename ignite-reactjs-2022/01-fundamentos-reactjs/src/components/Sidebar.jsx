ae5c040ae0be628c66fae84d5847383559f5611c
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-06-09 13:41:14 -0300

    refatorando: componente avatar

--- ignite-reactjs-2022/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ ignite-reactjs-2022/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar(){
@@ -10,7 +11,7 @@ export function Sidebar(){
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/lwkas-gabriel.png" alt="" />
+                <Avatar src="https://github.com/lwkas-gabriel.png"/>
 
                 <strong>Lucas Gabriel</strong>
                 <span>Web Developer</span>
