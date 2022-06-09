941b22508b21b1e5287ab3997d582f8e42e0810f
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-06-09 12:03:19 -0300

    componente lista de comentários

---
+++ ignite-reactjs-2022/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import {ThumbsUp, Trash} from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/lwkas-gabriel.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Lucas Gabriel</strong>
+                            <time title="07 de junho às 20:47h" dateTime="2022-06-07 20:47:25">Cerca de 1h atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+                
+            </div>
+        </div>
+    )
+}
