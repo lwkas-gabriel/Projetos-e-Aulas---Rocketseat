69ac4588626a250a05d28609d6b42c2c3cb0d95b
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-01 16:25:49 -0300

    componentes do form de feedback finalizados

--- nlw-return-reactjs/web/tailwind.config.js
+++ nlw-return-reactjs/web/tailwind.config.js
@@ -5,10 +5,16 @@ module.exports = {
     extend: {
       colors:{
         brand:{
+          300: "#996DFF",
           500: "#8257e6"
         }
+      },borderRadius:{
+        md:"4px",
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+  ],
 }
