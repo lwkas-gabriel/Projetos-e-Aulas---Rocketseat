db93304b88431166e1c8dec7157999961a6ddace
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-02 23:28:28 -0300

    finalizando feature screenshot

--- nlw-return-reactjs/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ nlw-return-reactjs/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -12,6 +13,7 @@ export function FeedbackContentStep(
     {feedbackType,
      onFeedbackRestartRequested
     }: FeedbackContentStepProps){
+    const [screenshot, setScreenshot] = useState<string|null>(null)
     const feedbackTypeInfo = feedbackTypes[feedbackType]
 
 
@@ -36,7 +38,10 @@ export function FeedbackContentStep(
                 />
 
                 <footer className="flex gap-2 mt-2">
-                    <ScreenshotButton/>
+                    <ScreenshotButton
+                     screenshot={screenshot}
+                     onScreenshotTook={setScreenshot}
+                    />
 
                     <button
                     type="submit"
