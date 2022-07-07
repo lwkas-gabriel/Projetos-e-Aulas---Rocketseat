9ea9693e8a39236868d0690aa46f6dd5658bb89e
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-07 16:32:48 -0300

    commit inicial

---
+++ ignite-reactjs-2022/desafios/todo-list/src/components/Header.tsx
@@ -0,0 +1,9 @@
+// import logo from "../assets/logo-todo.png"
+
+export function Header(){
+    return (
+        <div className="w-full h-52 bg-gray-700 flex justify-center">
+           
+        </div>
+    )
+}
