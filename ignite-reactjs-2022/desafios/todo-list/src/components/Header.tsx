1da28580aede5bd10cb4d6b6494e36582d440608
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-07 19:43:10 -0300

    iniciando componente TodoWidget

--- ignite-reactjs-2022/desafios/todo-list/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-// import logo from "../assets/logo-todo.png"
-
-export function Header(){
-    return (
-        <div className="w-full h-52 bg-gray-700 flex justify-center">
-           
-        </div>
-    )
-}
---
+++ ignite-reactjs-2022/desafios/todo-list/src/components/TodoWidget.tsx
@@ -0,0 +1,10 @@
+// import logo from "../assets/logo-todo.png"
+
+export function TodoWidget(){
+    return (
+        <div>
+            <input type="text" />
+            <button>Criar</button>
+        </div>
+    )
+}
