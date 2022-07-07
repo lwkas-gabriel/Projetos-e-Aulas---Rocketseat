a4b0f23da306063d1c5bfa38c1b3cd00c1219c7d
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-07 19:38:36 -0300

    adicionando cores de fonte

--- ignite-reactjs-2022/desafios/todo-list/tailwind.config.js
+++ ignite-reactjs-2022/desafios/todo-list/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        gray:{
+          200: "#D9D9D9",
+          300: "#808080",
+          400: "#333333",
+          500: "#262626",
+          600: "#1A1A1A",
+          700: "#0D0D0D"
+        },
+      }
+    },
   },
   plugins: [],
 }
