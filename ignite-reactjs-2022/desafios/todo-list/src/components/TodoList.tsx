8319ff77941d9c81717e9f8dff996e2dae356412
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-07-08 22:05:03 -0300

    cabeçalho TodoList

--- ignite-reactjs-2022/desafios/todo-list/src/components/TodoList.tsx
+++ ignite-reactjs-2022/desafios/todo-list/src/components/TodoList.tsx
@@ -1,11 +1,19 @@
 export function TodoList(){
     return (
-        <div>
-            <div>
-
+        <div className="mr-auto ml-auto mt-16 w-[736px]">
+            <div className="flex justify-between mb-6">
+                <div className="flex items-center gap-2">
+                    <span className="text-sm font-bold text-[#4EA8DE]">Tarefas criadas</span>
+                    <span className="text-xs font-bold text-gray-200"><div className="bg-gray-400 rounded-full px-2">0</div></span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <span className="text-sm font-bold text-[#8284FA]">Concluidas</span>
+                    <span className="text-xs font-bold text-gray-200"><div className="bg-gray-400 rounded-full px-[15px]">0 de 0</div></span>
+                </div>
             </div>
+
             <div>
-                
+                <input type="checkbox" name="teste"/>
             </div>
         </div>
     )
