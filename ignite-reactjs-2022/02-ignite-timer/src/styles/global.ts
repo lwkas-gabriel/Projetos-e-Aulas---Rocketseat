d3eb34e946202acd6ffc78f2b92af31b824c8e29
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-08-09 15:39:23 -0300

    finalizando pagina home

--- ignite-reactjs-2022/02-ignite-timer/src/styles/global.ts
+++ ignite-reactjs-2022/02-ignite-timer/src/styles/global.ts
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     :focus{
-        outline: 0;
+        outline: none;
         box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
     }
 
@@ -18,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    body,input,textarea,button{
+    body, input, textarea, button{
         font-family: "Roboto", sans-serif;
         font-weight: 400;
         font-size: 1rem;
