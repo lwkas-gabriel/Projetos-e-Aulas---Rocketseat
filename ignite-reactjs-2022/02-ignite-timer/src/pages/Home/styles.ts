0cec09d141640333d85d0a064c094071a514246f
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-08-10 14:11:18 -0300

    corrigindo bugs e finalizando página home

--- ignite-reactjs-2022/02-ignite-timer/src/pages/Home/styles.ts
+++ ignite-reactjs-2022/02-ignite-timer/src/pages/Home/styles.ts
@@ -23,7 +23,7 @@ export const FormContainer = styled.div`
   justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme['gray-100']};
-  font-size: 1.125rem;
+  font-size: 1.1rem;
   font-weight: bold;
   flex-wrap: wrap;
 `
@@ -49,6 +49,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
