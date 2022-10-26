030a3a7ddfa45e333d8b70a1a9ec2c6dc7e92529
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-10-26 20:22:49 -0300

    finalizando configuração ambiente node

--- nlw-esport-reactjs/server/src/server.ts
+++ nlw-esport-reactjs/server/src/server.ts
@@ -8,6 +8,7 @@ app.get('/ads', (request, response)=>{
             {id:1, nome:"teste 1"},
             {id:2, nome:"teste 2"},
             {id:3, nome:"teste 3"},
+            {id:4, nome:"ablublu"},
         ]
     )
 });
