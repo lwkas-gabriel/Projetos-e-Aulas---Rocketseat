ec83cbc4cddaabd348d14052f57efb446d8b5037
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-11-01 14:58:14 -0300

    configuração tailwind

--- nlw-esport-reactjs/web/src/App.tsx
+++ nlw-esport-reactjs/web/src/App.tsx
@@ -1,5 +1,11 @@
+import "./styles/main.css"
+
 function App() {
-  return <h1>teste</h1>
+  return (
+    <div className="w-8 h-8 bg-violet-500">
+      <h1>teste</h1>
+    </div>
+  )
 }
 
 export default App
