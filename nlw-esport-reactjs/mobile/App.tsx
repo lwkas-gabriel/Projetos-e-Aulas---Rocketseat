eb0a468b20947b9c1526466f750ca3003c5c2c67
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-11-08 16:27:12 -0300

    stage 2 - status bar mobile

--- nlw-esport-reactjs/mobile/App.tsx
+++ nlw-esport-reactjs/mobile/App.tsx
@@ -1,10 +1,15 @@
 import {View } from 'react-native';
 import { Background } from './src/components/Background';
+import {StatusBar} from 'react-native'
 
 export default function App() {
   return (
     <Background>
-      
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </Background>
   );
 }
