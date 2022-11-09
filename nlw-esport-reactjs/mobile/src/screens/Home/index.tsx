ce4c2e51e59605f894d1fd4fedc48ec32dea6105
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-11-09 14:31:24 -0300

    stage 2 - inserindo logo mobile

--- nlw-esport-reactjs/mobile/src/screens/Home/index.tsx
+++ nlw-esport-reactjs/mobile/src/screens/Home/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { View } from 'react-native';
-
+import { View, Image } from 'react-native';
 import { styles } from './styles';
+
+import logoImage from '../../assets/logo-nlw-esports.png'
 
 export function Home() {
   return (
     <View style={styles.container}>
-
+      <Image
+        source={logoImage}
+        style={styles.logo}
+      />
     </View>
   );
 }
