c3f8119ac97f0336e7a5f6c6564abf850632ff25
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-07 12:25:01 -0300

    instalando cors

--- nlw-setup-reactjs/server/src/server.ts
+++ nlw-setup-reactjs/server/src/server.ts
@@ -1,16 +1,17 @@
-import Fastify from 'fastify'
+import Fastify from "fastify"
+import cors from "@fastify/cors" 
 import {PrismaClient} from "@prisma/client"
 
 const app = Fastify();
 const prisma = new PrismaClient();
 
-app.get("/teste", async () => {
+app.register(cors);
+
+app.get("/", async () => {
     //se eu tiro o where, ele retorna todas as entradas do banco de dados
     const habits = await prisma.habit.findMany({
         where: {
-            title:{
-                startsWith: "Beber"
-            }
+
         }
     })
     return habits
