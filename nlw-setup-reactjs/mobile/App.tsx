4b49431e500b8b857744d491f2227fcffbd7e096
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-07 18:32:57 -0300

    carregando fontes

--- nlw-setup-reactjs/mobile/App.tsx
+++ nlw-setup-reactjs/mobile/App.tsx
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from "@expo-google-fonts/inter"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded){
+    return;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
