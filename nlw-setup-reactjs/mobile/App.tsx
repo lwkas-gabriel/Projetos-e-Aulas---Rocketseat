1b62b1bb58d4e20f6b54016439bef5edc425fcab
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-07 18:53:40 -0300

    adicionando componente loading e bug fix

--- nlw-setup-reactjs/mobile/App.tsx
+++ nlw-setup-reactjs/mobile/App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from "@expo-google-fonts/inter"
 
+import { Loading } from './src/components/Loading';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -11,7 +13,9 @@ export default function App() {
   });
 
   if (!fontsLoaded){
-    return;
+    return (
+      <Loading/>
+    )
   }
 
   return (
@@ -25,11 +29,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: "red"
+    color: "#fff",
+    fontFamily: "Inter_800ExtraBold"
   }
 });
