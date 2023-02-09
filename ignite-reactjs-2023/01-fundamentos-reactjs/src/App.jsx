271ed7f1f8115cfd4eb0ef3951556fa2d37faa08
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-09 15:47:25 -0300

    componente sidebar, post e biblioteca phosphor

--- ignite-reactjs-2023/01-fundamentos-reactjs/src/App.jsx
+++ ignite-reactjs-2023/01-fundamentos-reactjs/src/App.jsx
@@ -1,4 +1,8 @@
 import { Header } from "./components/Header"
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -6,6 +10,21 @@ function App() {
   return (
     <div>
       <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+          author="test 1"
+          content="Novo post 1"
+          />
+
+          <Post
+          author="test 2"
+          content="Novo post 2"
+          />
+        </main>
+      </div>
     </div>
   )
 }
