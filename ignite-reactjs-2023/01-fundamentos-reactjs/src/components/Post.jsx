e1c104d64690c811b271ea1100b123a42d2c4b15
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-10 17:25:11 -0300

    componente post finalizado

--- ignite-reactjs-2023/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite-reactjs-2023/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,8 +1,30 @@
+import styles from './Post.module.css'
+
 export function Post(props){
     return (
-        <div>
-            <strong>{props.author}</strong>
-            <p>{props.content}</p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img src="https://github.com/lwkas-gabriel.png" alt="" className={styles.avatar}/>
+                    <div className={styles.authorInfo}>
+                        <strong>Lucas Gabriel</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="10 de Fevereiro às 15:40" dataTime="2023-02-10 15:40">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{" "}
+                    <a href="">#nlw</a>{" "}
+                    <a href="">#rocketseat</a>{" "}
+                </p>
+            </div>
+        </article>
     )
 }
