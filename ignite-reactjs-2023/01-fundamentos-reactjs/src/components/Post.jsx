1e0aea6f44e0ad57a34504c7c9593966925935c4
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-10 23:26:25 -0300

    finalizando post com formulario de postagens

--- ignite-reactjs-2023/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite-reactjs-2023/01-fundamentos-reactjs/src/components/Post.jsx
@@ -25,6 +25,16 @@ export function Post(props){
                     <a href="">#rocketseat</a>{" "}
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                  placeholder='Deixe um comentário
+                '/>
+                <footer>
+                    <button type='submit'>Comentar</button>    
+                </footer>
+            </form>
         </article>
     )
 }
