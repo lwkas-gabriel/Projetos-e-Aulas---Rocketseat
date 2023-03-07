561b9acfec72f94dd4eb85502475b5097ea2b927
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-03-06 21:35:10 -0300

    introdução e configurando rotas

--- ignite-reactjs-2023/03-ignite-timer/src/App.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/App.tsx
@@ -1,15 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>teste</h1>
-
-      <GlobalStyle/>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+        <GlobalStyle/>
     </ThemeProvider>
   )
 }
