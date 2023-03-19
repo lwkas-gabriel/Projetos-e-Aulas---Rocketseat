39ec5fb2804378195fffbe7880b58f41a33786c4
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-03-19 18:54:12 -0300

    valicação de form usando zod+react hook form

--- ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
@@ -1,13 +1,38 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod'
+import zod from 'zod'
+
 import { HomeContainer, FormContainer, CountdownContainer, Separator, StartCountdownButton, MinutesAmountInput, TaskInput } from './styles';
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    MinutesAmount: zod.number().min(5).max(60),
+})
+
 export function Home(){
+    const { register, handleSubmit, watch, formState } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    });
+
+    function handleCreateNewCicle(data:any){
+
+    }
+
+    const task = watch('task');
+    const isSubmitDisabled = !task;
+
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" list="task-suggestions" placeholder='Dê um nome para o seu projeto'/>
+                    <TaskInput id="task"
+                     list="task-suggestions"
+                     placeholder='Dê um nome para o seu projeto'
+                     {...register('task'), {valueAsNumber: true}}
+                    />
 
                     <datalist id="task-suggestions">
                         <option value="Projeto 1"/>
@@ -37,7 +62,7 @@ export function Home(){
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled type="submit">
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24}/>
                     Começar
                 </StartCountdownButton>
