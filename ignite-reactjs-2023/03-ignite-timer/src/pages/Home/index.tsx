3ab4c292529204ca016d96167d639e47492e1913
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-03-23 20:38:19 -0300

    implementando funcionalidade de ciclo completo

--- ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
@@ -28,6 +28,8 @@ interface Cycle{
     task: string;
     minutesAmount: number;
     start: Date;
+    interruptedDate?: Date;
+    finishedDate?: Date;
 }
 
 export function Home(){
@@ -45,20 +47,35 @@ export function Home(){
 
     const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
 
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount*60 : 0;
+
     useEffect(()=>{
         let interval: number;
         if(activeCycle){
             interval = setInterval(()=>{
-                setAmountSecondsPassed(
-                    differenceInSeconds(new Date(), activeCycle.start)
-                )
+                const secondsDifference = differenceInSeconds(new Date(), activeCycle.start)
+
+                if(secondsDifference>=totalSeconds){
+                    setCycles( state => state.map((cycle) => {
+                        if(cycle.id === activeCycleId){
+                            return {...cycle, finishedDate: new Date() }
+                        } else{
+                            return cycle;
+                        }
+                    }),
+                    )
+                    setAmountSecondsPassed(totalSeconds);
+                    clearInterval(interval);
+                }else{
+                    setAmountSecondsPassed(secondsDifference)
+                }
             }, 1000)
         }
 
         return () => {
             clearInterval(interval);
         }
-    },[activeCycle])
+    },[activeCycle, totalSeconds, activeCycleId])
 
     function handleCreateNewCycle(data: NewCycleFormData){
         const newCycle: Cycle = {
@@ -77,18 +94,17 @@ export function Home(){
     function handleInterruptCycle(){
         setActiveCycleId(null);
 
-        setCycles(
-            cycles.map((cycle) => {
-            if(cycle.id === activeCycleId){
-                return {...cycle, interruptedDate: new Date()}
-            } else{
-                return cycle;
-            }
-            }),
+        setCycles((state) => 
+            state.map((cycle) => {
+                if(cycle.id === activeCycleId){
+                    return {...cycle, interruptedDate: new Date()}
+                } else{
+                    return cycle;
+                }
+                }),
         );
     }
 
-    const totalSeconds = activeCycle ? activeCycle.minutesAmount*60 : 0;
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
     const minutesAmount = Math.floor(totalSeconds / 60);
