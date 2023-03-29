e8d71a9a5fbb9da2de15a1ed49d3ea69fce6003b
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-03-28 22:19:05 -0300

    refatorando aplicação para usar ContextAPI

--- ignite-reactjs-2023/03-ignite-timer/src/pages/Home/components/Countdown/index.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,56 @@
+import { differenceInSeconds } from "date-fns";
+import { useContext, useEffect, useState } from "react";
+import { CyclesContext } from "../..";
 import { CountdownContainer, Separator } from "./styles";
 
 export function Countdown(){
+    const { activeCycle, activeCycleId, markCurrentCycleAsFinished } = useContext(CyclesContext);
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount*60 : 0;
+
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+    const minutesAmount = Math.floor(totalSeconds / 60);
+    const secondsAmount = currentSeconds % 60;
+
+    const minutes = String(minutesAmount).padStart(2, '0');
+    const seconds = String(secondsAmount).padStart(2, '0');
+
+    useEffect(()=>{
+        if(activeCycle)
+            document.title = `${minutes}:${seconds}`
+    },[minutes, seconds, activeCycle])
+
+    useEffect(()=>{
+        let interval: number;
+        if(activeCycle){
+            interval = setInterval(()=>{
+                const secondsDifference = differenceInSeconds(new Date(), activeCycle.start)
+
+                if(secondsDifference>=totalSeconds){
+                    // setCycles( state => 
+                    //   state.map((cycle) => {
+                    //     if(cycle.id === activeCycleId){
+                    //         return {...cycle, finishedDate: new Date() }
+                    //     } else{
+                    //         return cycle;
+                    //     }
+                    // }),
+                    // )
+                    markCurrentCycleAsFinished();
+                    setAmountSecondsPassed(totalSeconds);
+                    clearInterval(interval);
+                }else{
+                    setAmountSecondsPassed(secondsDifference)
+                }
+            }, 1000)
+        }
+
+        return () => {
+            clearInterval(interval);
+        }
+    },[activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+
     return (
         <CountdownContainer>
             <span>{minutes[0]}</span>
