e4a37c31735f301e07b1551ff154e6b9c75b736b
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-06-15 20:15:37 -0300

    feat: validando formulário de adição de novo comentário

--- ignite-reactjs-2023/01-fundamentos-reactjs/src/components/Post.jsx
+++ ignite-reactjs-2023/01-fundamentos-reactjs/src/components/Post.jsx
@@ -21,7 +21,12 @@ export function Post({author, content, publishedAt}){
     });
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity("Esse campo é obrigatório");
     }
 
     function handleCreateNewComment(){
@@ -36,6 +41,8 @@ export function Post({author, content, publishedAt}){
         });
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -71,10 +78,14 @@ export function Post({author, content, publishedAt}){
                     placeholder="Deixe um comentário"
                     onChange={handleNewCommentChange}
                     value={newCommentText}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
