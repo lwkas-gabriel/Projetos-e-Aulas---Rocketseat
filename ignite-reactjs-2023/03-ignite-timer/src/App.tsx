4246a7b595b0e31efd35d4331adea7804a1178f5
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-06-26 18:43:07 -0300

    inicando aula de styled components

--- ignite-reactjs-2023/03-ignite-timer/src/App.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Teste</h1>
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+    </>
   )
 }
