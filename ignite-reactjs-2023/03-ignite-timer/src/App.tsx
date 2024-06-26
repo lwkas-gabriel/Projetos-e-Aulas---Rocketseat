9eaa3218cef7b25b7647e60872125dac71418206
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-06-26 17:02:56 -0300

    feat: reiniciando o projeto

--- ignite-reactjs-2023/03-ignite-timer/src/App.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/App.tsx
@@ -1,19 +1,5 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
-import { Router } from './Router'
-import { CyclesContextProvider } from './contexts/CyclesContext'
-
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <CyclesContextProvider>
-          <Router/>
-        </CyclesContextProvider>
-      </BrowserRouter>
-        <GlobalStyle/>
-    </ThemeProvider>
+    <h1>Teste</h1>
   )
 }
