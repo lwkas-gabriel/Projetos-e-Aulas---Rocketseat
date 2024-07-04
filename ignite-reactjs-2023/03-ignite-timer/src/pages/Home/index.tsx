61a617f5122bc933d055f6bfd9d0384f515b246c
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-04 19:03:43 -0300

    feat: validação de form com a biblioteca zod

--- ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
@@ -1,10 +1,27 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
-import { FormContainer, HomeContainer, CountdownContainer, Separator, StartCountdownButton, TasKInput, MinutesAmountInput } from "./styles";
-import { useState } from "react";
+import { FormContainer,
+         HomeContainer,
+         CountdownContainer,
+         Separator,
+         StartCountdownButton, 
+         TasKInput, 
+         MinutesAmountInput } from "./styles";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod
+        .number()
+        .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+        .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+})
 
 export function Home(){
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch, formState  } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    });
 
     function handleCreateNewCycle(data: any){
 
