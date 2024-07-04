711db802a85c190fca271303321e4b50719fd5ad
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-07-04 16:55:50 -0300

    feat: melhorando o formulário com react hook form

--- ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
+++ ignite-reactjs-2023/03-ignite-timer/src/pages/Home/index.tsx
@@ -1,16 +1,28 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { FormContainer, HomeContainer, CountdownContainer, Separator, StartCountdownButton, TasKInput, MinutesAmountInput } from "./styles";
+import { useState } from "react";
 
 export function Home(){
+    const { register, handleSubmit, watch } = useForm();
+
+    function handleCreateNewCycle(data: any){
+
+    }
+
+    const task = watch("task");
+    const isSubmitDisabled = !task;
+
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}>
                 <FormContainer>
                     <label htmlFor="">Vou trabalhar em</label>
                     <TasKInput
                         id="task"
                         list="task-suggestions"
                         placeholder="Dê um nome para o seu projeto"
+                        {...register("task")}
                     />
 
                     <datalist id="task-suggestions">
@@ -28,6 +40,7 @@ export function Home(){
                         step={5}
                         min={5}
                         max={60}
+                        {...register("minutesAmount", { valueAsDate: true })}
                     />
 
                     <span>minutos.</span>
@@ -41,7 +54,7 @@ export function Home(){
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled type="submit">
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play />
                     Começar
                 </StartCountdownButton>
