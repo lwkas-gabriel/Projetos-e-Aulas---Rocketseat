cea9f4b5d2a39053b9ea22bb5fa9a828b141e5f3
lwkas-gabriel <lwkasgabriel@hotmail.com>
2025-08-29 18:58:28 -0300

    chore(turbo-repo): instalando e configurando o turbo repo

--- ignite-reactjs-2023/05-design-system/packages/docs/vite.config.ts
+++ ignite-reactjs-2023/05-design-system/packages/docs/vite.config.ts
@@ -6,7 +6,10 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// Corrigindo __dirname em ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -15,23 +18,22 @@ export default defineConfig({
     projects: [{
       extends: true,
       plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
+        storybookTest({
+          configDir: path.join(__dirname, '.storybook'),
+        }),
+      ],
       test: {
         name: 'storybook',
         browser: {
           enabled: true,
           headless: true,
           provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
+          instances: [
+            { browser: 'chromium' },
+          ],
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+        setupFiles: ['.storybook/vitest.setup.ts'],
+      },
+    }],
+  },
 });
